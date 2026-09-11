04be2f98d5438fd24ffa3ff14a2ebf4c9fe8bc0d
abdul Rafay <abdulrafay700000@gmail.com>
2026-09-11 23:53:16 +0500

    backend: reset-password url create in AppServiceProvider

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -20,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-       
+        ResetPassword::createUrlUsing(function ($user, string $token) {
+           return config('app.frontend_url')
+               . '/reset-password?token=' . $token
+               . '&email=' . urlencode($user->email);
+        });
     }
 }
